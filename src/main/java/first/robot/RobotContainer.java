cc2a632af99b421677ac50990c1d99bd8c238e3f
ElliotScher <ecscher84@gmail.com>
2026-08-17 19:15:50 -0400

    correct package declarations

--- src/main/java/first/robot/RobotContainer.java
+++ src/main/java/first/robot/RobotContainer.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package org.wpilib.templates.commandv2skeleton;
+package first.robot;
 
 import org.wpilib.command2.Command;
 import org.wpilib.command2.Commands;
