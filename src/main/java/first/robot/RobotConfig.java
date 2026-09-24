da288573e8a804928a05cacc073fb9404d962f07
jasminepalit <jasmine.palit@gmail.com>
2026-09-24 18:15:10 -0400

    sim mode robot

--- src/main/java/first/robot/RobotConfig.java
+++ src/main/java/first/robot/RobotConfig.java
@@ -1,7 +1,7 @@
 package first.robot;
 
 public final class RobotConfig {
-  public static final RobotType ROBOT = RobotType.V3_Horse_CV2_TR;
+  public static final RobotType ROBOT = RobotType.V3_Horse_CV2_TR_SIM;
 
   public enum RobotType {
     V3_Horse_CV2_TR,
